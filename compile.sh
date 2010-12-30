07c73636ee3d9930b933e6a4e8cf861ae2978d1a
Francisco Coelho <eng.frcoelho@gmail.com>
2010-12-30 08:33:02 -0400

    Fix compile.sh script
    The compile.sh script was not creating the pesti binary if build
    directory did not exist.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This script builds executable file "pesti"
 
+# Create build directory if it does not exist
+mkdir -p build
+
+# Clean old binaries
 rm -f build/*
+
 gcc -I /usr/include/libxml2/ -I include/ -L /usr/lib/ -o build/pesti src/simul-engine.c src/control-flow-graph.c src/random-number.c src/monte-carlo.c src/main.c -lxml2
 
 
